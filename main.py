c5eb08c7353b465888ac566119c0a35644fb5451
chris <chris.g.harvey@gmail.com>
2024-04-29 22:14:53 -0500

    added first character!!!! his name is deadeye btw

--- main.py
+++ main.py
@@ -5,25 +5,25 @@ import sys
 
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
-pygame.display.set_caption("ETERNAL EMPIRE")
+pygame.display.set_caption("eternal empire")
 manager=pygame_gui.UIManager ((640 , 480))
 start=pygame_gui.elements.UIButton(pygame.Rect((350, 275), (100,50)),'hi', manager)
 
 running = True
 
 screen.fill((255, 0, 0))
-elliotspriteimage=pygame.image.load('sprites/elliotposter.png')
-scaledelliotposter=pygame.transform.scale(elliotspriteimage,(32, 32))
+deadeye=pygame.image.load('sprites/deadeye_1.png')
+scaleddeadeye_1=pygame.transform.scale(deadeye,(100, 100))
 x=0
 y=0
 direction=1
-font=pygame.font.Font(None, 100)
-text=font.render('how did u see this', True, (0, 0 ,0))
+font=pygame.font.Font(None, 60)
+text=font.render('deadeye s place', True, (0, 0 ,0))
 
 while running:
     screen.fill((140, 140, 140))
     screen.blit(text,(3 , 3))
-    screen.blit(scaledelliotposter,(x, y))
+    screen.blit(scaleddeadeye_1,(x, y))
     manager.draw_ui(screen)
 
     pygame.display.flip()
@@ -44,10 +44,10 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        y=y-0.1
+        y=y-0.07
     if keys[pygame.K_s]:
-        y=y+0.1
+        y=y+0.07
     if keys[pygame.K_d]:
-        x=x+0.1
+        x=x+0.07
     if keys[pygame.K_a]:
-        x=x-0.1
+        x=x-0.07
